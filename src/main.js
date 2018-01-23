373facab8889aff78b8383b88cc91ecaf3c6a220
marcpre <marcus.presich@gmail.com>
2018-01-23 15:45:35 +0000

    fixing pulp fiction component

--- src/main.js
+++ src/main.js
@@ -4,12 +4,22 @@ import './style.scss';
 new Vue({
   el: '#app',
   components: {
-    msg: 'Hello World',
     'movie-list': {
-      template: `<div id="movie-list"> Movie List </div>`
+      template: `<div id="movie-list"> 
+        <div v-for="movie in movies">{{ movie.title }}</div>
+      </div>`,
+      data() {
+        return {
+          movies: [
+            { title: "Pulp Fiction" },
+            { title: "Pulp Fiction" },
+            { title: "Pulp Fiction" }
+          ]
+        }
+      },
     },
     'movie-filter': {
-      template: `<div id="movie-list"> Movie Filter </div>`
+      template: `<div id="movie-filter"> Movie Filter </div>`,
     }
-  }
+  },
 });
