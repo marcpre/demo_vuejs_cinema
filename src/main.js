12173656b96e55df1aaca4571bb42fae20b7bcb7
marcpre <marcus.presich@gmail.com>
2018-01-23 16:06:25 +0000

    adding filter template

--- src/main.js
+++ src/main.js
@@ -1,12 +1,13 @@
 import Vue from 'vue';
 import './style.scss';
+import genres from './util/genres.js';
 
 new Vue({
   el: '#app',
   components: {
     'movie-list': {
       template: `<div id="movie-list"> 
-        <div v-for="movie in movies">{{ movie.title }}</div>
+        <div v-for="movie in movies" class="movie">{{ movie.title }}</div>
       </div>`,
       data() {
         return {
@@ -19,7 +20,23 @@ new Vue({
       },
     },
     'movie-filter': {
-      template: `<div id="movie-filter"> Movie Filter </div>`,
+      data() {
+        return {
+          genres
+        }
+      },
+      template: `
+      <div id="movie-filter"> 
+        <h2>Filter Result<h2> 
+        <div class="filter-group">
+          <check-filter v-for="genre in genres">{{ genre }}</check-filter>
+        </div>
+      </div>`,
+      components: {
+        'check-filter': {
+          template: `<div>filter</div>`,
+        }
+      }
     }
   },
 });
