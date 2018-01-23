f0ab4b7bc99313b4d34d9beea2fe96aecbb4fe1e
marcpre <marcus.presich@gmail.com>
2018-01-23 07:19:54 +0000

    starting components

--- src/main.js
+++ src/main.js
@@ -2,8 +2,14 @@ import Vue from 'vue';
 import './style.scss';
 
 new Vue({
-    el: '#app',
-    data: {
-      msg: 'Hello World'  
+  el: '#app',
+  components: {
+    msg: 'Hello World',
+    'movie-list': {
+      template: `<div id="movie-list"> Movie List </div>`
+    },
+    'movie-filter': {
+      template: `<div id="movie-list"> Movie Filter </div>`
     }
+  }
 });
