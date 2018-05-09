b5af8c480ecd72e19a992db9542d6a8897e0fc7e
marcpre <marcus.presich@gmail.com>
2018-05-09 04:39:10 +0000

    adding no results msg

--- src/components/MovieList.vue
+++ src/components/MovieList.vue
@@ -1,6 +1,14 @@
 <template>
     <div id="movie-list"> 
-          <movie-item v-for="movie in filteredMovies" class="movie" v-bind:movie="movie.movie"></movie-item>
+        <div v-if="filteredMovies.length">
+              <movie-item v-for="movie in filteredMovies" class="movie" v-bind:movie="movie.movie"></movie-item>    
+        </div>
+        <div v-else-if="movies.length" class="no-results">
+            No results.
+        </div>
+        <div v-else class="no-results">
+            Loading...
+        </div>
     </div>
 </template>
 <script>
@@ -15,7 +23,14 @@ import MovieItem from './MovieItem.vue'
                     return true
                 }
                 else {
-                    return this.genre.find(genre => movie.genre === genre)
+                    let movieGenres = movie.movie.Genre.split(", ")
+                    let matched = true
+                    this.genre.forEach(genre => {
+                        if(movieGenres.indexOf(genre) === -1) {
+                            matched = false
+                        }
+                        return matched
+                    })
                 }
             }
         },
