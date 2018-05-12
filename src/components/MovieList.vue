ed1707aafdc866d499bca7d3403f1e10549ed831
marcpre <marcus.presich@gmail.com>
2018-05-12 06:19:53 +0000

    adding time filter

--- src/components/MovieList.vue
+++ src/components/MovieList.vue
@@ -15,8 +15,9 @@
     </div>
 </template>
 <script>
-import genres from '../util/genres';
-import MovieItem from './MovieItem.vue'
+    import genres from '../util/genres';
+    import times from '../util/times'
+    import MovieItem from './MovieItem.vue'
 
     export default {
         props: ['genre', 'time', 'movies', 'day'],
@@ -29,17 +30,33 @@ import MovieItem from './MovieItem.vue'
                     let movieGenres = movie.movie.Genre.split(", ")
                     let matched = true
                     this.genre.forEach(genre => {
-                        if(movieGenres.indexOf(genre) === -1) {
+                        if (movieGenres.indexOf(genre) === -1) {
                             matched = false
                         }
                         return matched
                     })
                 }
+            },
+            sessionPassesTimeFilter(session) {
+                if (!this.day.isSame(this.$moment(session.time), 'day')) {
+                    return false
+                }
+                else if (this.time.length === 0 || this.time.length === 2) {
+                    return true
+                }
+                else if (this.time[0] === times.AFTER_6PM) {
+                    return this.$moment(session.time).hour() >= 18
+                }
+                else {
+                    return this.$moment(session.time).hour() < 18
+                }
             }
         },
         computed: {
             filteredMovies() {
-                return this.movies.filter(this.moviePassesGenreFilter)
+                return this.movies
+                    .filter(this.moviePassesGenreFilter)
+                    .filter(movie => movie.sessions.find(this.sessionPassesTimeFilter))
             }
         },
         components: {
