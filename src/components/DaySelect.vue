a5f9f665c9376889257b76ca45650a2c5c120199
marcpre <marcus.presich@gmail.com>
2018-05-16 04:22:23 +0000

    adding selected day

--- src/components/DaySelect.vue
+++ src/components/DaySelect.vue
@@ -1,7 +1,9 @@
 <template>
     <div id="day-select">
         <ul class="days">
-            <li class="day" v-for="day in days">{{ formatDay(day) }}</li>
+            <li v-bind:class="{ day: true, active: isActive(day) }" v-for="day in days" v-on:click="selected = day">
+                {{ formatDay(day) }}
+            </li>
         </ul>
     </div>
 </template>
@@ -9,12 +11,20 @@
     export default {
         data() {
             return {
+                selected: this.$moment(),
                 days: [0, 1, 2, 3, 4, 5, 6].map(num => this.$moment().add(num, 'days'))
             }
         },
         methods: {
             formatDay(raw) {
-                return raw.format('ddd DD/MM')
+                if(raw.isSame(this.$moment(), 'day')) {
+                    return 'Today'
+                } else {
+                    return raw.format('ddd DD/MM')
+                }
+            },
+            isActive(day) {
+                return day.isSame(this.selected, 'day')
             }
         }
     }
